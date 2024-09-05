8bff3f30b9ab78223254d6fa5f1a13a118b937ac
ahearndylan <dahearn2021@gmail.com>
2024-09-04 20:08:20 -0400

    Fix donation slideshow

--- static/js/donate-carousel.js
+++ static/js/donate-carousel.js
@@ -3,27 +3,4 @@ $('#myCarousel').carousel({
     wrap: true        // Allow looping of slides
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    const carousel = document.querySelector('#myCarousel');
-    const slides = carousel.querySelectorAll('.carousel-item');
-    
-    // Function to update the scaling and opacity based on the active slide
-    function updateSlides() {
-        slides.forEach((slide, index) => {
-            slide.classList.remove('prev-slide', 'next-slide');
-            if (slide.classList.contains('active')) {
-                slide.style.transform = 'scale(1.2)';
-                slide.style.opacity = '1';
-            } else {
-                slide.style.transform = 'scale(0.8)';
-                slide.style.opacity = '0.5';
-            }
-        });
-    }
-    
-    // Manually update the scaling after each slide transition
-    carousel.addEventListener('slid.bs.carousel', updateSlides);
 
-    // Initialize the slide scaling on page load
-    updateSlides();
-});
